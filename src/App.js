5143282b3e0a8c8c90b6d20b75e94f64904ce292
specialobyno <specialobyno@yahoo.com>
2025-09-21 18:49:35 +0100

    added multiple images

--- src/App.js
+++ src/App.js
@@ -2,21 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const images = [1,2,3]
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          {images.map((image) => (
+            <img src={logo} className="App-logo" alt="logo" key={image} />
+          ))}
+        </div>
       </header>
     </div>
   );
